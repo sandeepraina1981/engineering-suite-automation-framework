5567b86d1daf83964f732fc25e683f206662b8e3
sandeepraina-lenze <75639388+sandeepraina-dsd19@users.noreply.github.com>
2024-10-12 22:33:21 +0530

    Changes in repo

--- src/test/java/org/lenze/nupano/suite/LenzeSuiteTestExecutor.java
+++ src/test/java/org/lenze/nupano/suite/LenzeSuiteTestExecutor.java
@@ -4,14 +4,16 @@ import io.cucumber.java.BeforeAll;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.lenze.nupano.suite.helpers.SuiteElementLoader;
+import org.lenze.nupano.suite.properties.SuiteProperties;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
 public class LenzeSuiteTestExecutor {
-//    @BeforeAll
-//    public static void LoadExecutor() {
-//        SuiteElementLoader suiteElementLoader = new SuiteElementLoader();
-//        SuiteProperties.suitePageElements = suiteElementLoader.getSuitePageElements();
-//    }
+    @BeforeAll
+    public static void LoadExecutor() {
+        SuiteElementLoader suiteElementLoader = new SuiteElementLoader();
+        SuiteProperties.suitePageElements = suiteElementLoader.getSuitePageElements();
+    }
 }
